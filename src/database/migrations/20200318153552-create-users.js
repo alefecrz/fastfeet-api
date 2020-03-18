b6c3522e724250575af38215cf615d63d2c2c24c
Álefe Cruz <alefecrz@gmail.com>
2020-03-18 12:46:32 -0300

    Configurando Migration e seeds

--- src/database/migrations/20200318153552-create-users.js
+++ src/database/migrations/20200318153552-create-users.js
@@ -1,6 +1,34 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
   },
 
   down: queryInterface => {
