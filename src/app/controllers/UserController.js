740cb8ae30baadcfda7569c223bcf01eaa6312de
Álefe Cruz <alefecrz@gmail.com>
2020-03-20 00:45:03 -0300

    Criado store de User com validação de entrada.

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,36 @@
+import * as Yup from 'yup';
+
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.json({ error: 'Validation fails' });
+    }
+
+    const userExist = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExist) {
+      return res.status(400).json({ error: 'User alredy exist!' });
+    }
+
+    const { id, name, email } = await User.create(req.body);
+
+    return res.json({ id, name, email });
+  }
+}
+
+export default new UserController();
